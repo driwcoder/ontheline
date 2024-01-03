7d8fb3e44676c36a162a5ffd49a063d8e98ac290
driwcoder <driwcoder@gmail.com>
2024-01-03 19:39:36 -0300

    ajeitando padding do conteúdo dos cursos

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const CoursesPage = () => {
   return (
-    <div className="p-6">
+    <div>
       <Link href={"/teacher/create"}>
         <Button>Novo curso</Button>
       </Link>
