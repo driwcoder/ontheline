005def9fa580a533a12a72225dfcfe259df30b60
driwcoder <driwcoder@gmail.com>
2024-01-25 00:08:46 -0300

    capítulos do curso e vídeo do curso

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import axios from "axios";
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { formatPrice } from "@/lib/format";
+import { toast } from "@/components/ui/use-toast";
+
+interface CourseEnrollButtonProps {
+  price: number;
+  courseId: string;
+}
+
+export const CourseEnrollButton = ({
+  price,
+  courseId,
+}: CourseEnrollButtonProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+
+      const response = await axios.post(`/api/courses/${courseId}/checkout`)
+
+      window.location.assign(response.data.url);
+    } catch {
+      toast({
+        title: "Algo errado aconteceu",
+        description: " O capítulo deu trinca",
+        variant: "destructive"
+      })
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <Button
+      onClick={onClick}
+      disabled={isLoading}
+      size="sm"
+      className="w-full md:w-auto"
+    >
+      Adquira por {formatPrice(price)}
+    </Button>
+  )
+}
---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import axios from "axios";
+import MuxPlayer from "@mux/mux-player-react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { Loader2, Lock } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { useConfettiStore } from "@/use-confetti-store";
+import { toast } from "@/components/ui/use-toast";
+
+interface VideoPlayerProps {
+  playbackId: string;
+  courseId: string;
+  chapterId: string;
+  nextChapterId?: string;
+  isLocked: boolean;
+  completeOnEnd: boolean;
+  title: string;
+};
+
+export const VideoPlayer = ({
+  playbackId,
+  courseId,
+  chapterId,
+  nextChapterId,
+  isLocked,
+  completeOnEnd,
+  title,
+}: VideoPlayerProps) => {
+  const [isReady, setIsReady] = useState(false);
+  const router = useRouter();
+  const confetti = useConfettiStore();
+
+  const onEnd = async () => {
+    try {
+      if (completeOnEnd) {
+        await axios.put(`/api/courses/${courseId}/chapters/${chapterId}/progress`, {
+          isCompleted: true,
+        });
+
+        if (!nextChapterId) {
+          confetti.onOpen();
+        }
+
+        toast({
+          title: "Parabéns",
+          description: "Progresso atualizado",
+          variant: "success"
+        })
+        router.refresh();
+
+        if (nextChapterId) {
+          router.push(`/courses/${courseId}/chapters/${nextChapterId}`)
+        }
+      }
+    } catch {
+      toast({
+        title: "Erro!",
+        description: "Algo deu errado",
+        variant: "destructive"
+      })
+    }
+  }
+
+  return (
+    <div className="relative aspect-video">
+      {!isReady && !isLocked && (
+        <div className="absolute inset-0 flex items-center justify-center bg-slate-800">
+          <Loader2 className="h-8 w-8 animate-spin text-secondary" />
+        </div>
+      )}
+      {isLocked && (
+        <div className="absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary">
+          <Lock className="h-8 w-8" />
+          <p className="text-sm">
+            This chapter is locked
+          </p>
+        </div>
+      )}
+      {!isLocked && (
+        <MuxPlayer
+          title={title}
+          className={cn(
+            !isReady && "hidden"
+          )}
+          onCanPlay={() => setIsReady(true)}
+          onEnded={onEnd}
+          autoPlay
+          playbackId={playbackId}
+        />
+      )}
+    </div>
+  )
+}
